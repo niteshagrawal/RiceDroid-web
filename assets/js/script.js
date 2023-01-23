50e4b789143195bf1bc4d19ffabf0698b298d429
NiteshAlgo <nitesh.agarwal@gmail.com>
2023-01-23 16:02:51 +0530

    make navbar stay after scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,25 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
   }
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('nav_bar').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+        document.getElementById('nav_bar').style.backgroundColor = '#101010';
+      } else {
+        document.getElementById('nav_bar').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+        document.getElementById('nav_bar').style.backgroundColor = 'transparent';
+      } 
+  });
+}); 
